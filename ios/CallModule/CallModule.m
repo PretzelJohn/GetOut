be8c332f742af900b3b781cc77dfe83946ab3555
nicknguyenl <92128336+nicknguyenl@users.noreply.github.com>
2022-11-13 21:02:33 -0500

    Co-authored-by: PretzelJohn <PretzelJohn@users.noreply.github.com>

---
+++ ios/CallModule/CallModule.m
@@ -0,0 +1,8 @@
+//
+//  CallModule.m
+//  GetOut
+//
+//  Created by Nicholas Nguyen on 11/13/22.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/CallModule/CallModule.swift
@@ -0,0 +1,8 @@
+//
+//  CallModule.swift
+//  GetOut
+//
+//  Created by Nicholas Nguyen on 11/13/22.
+//
+
+import Foundation
